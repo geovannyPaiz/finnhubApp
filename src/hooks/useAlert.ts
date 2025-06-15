619443669f900db6add59d00b5183cd5c1537f46
Samuel Paiz <geovanny_paiz@hotmail.com>
2025-06-15 11:00:36 -0600

    add:alert

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -1,9 +1,11 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { alertSchema } from '../utils/validations';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addAlert } from '../store/slice/alertsSlice';
 import { addWatchList } from '../store/slice/watchlistSlice';
+import { RootState } from '../store';
+import Toast from 'react-native-simple-toast';
 
 export const useAlert = () => {
   const dispatch = useDispatch();
@@ -15,8 +17,14 @@ export const useAlert = () => {
   } = useForm({
     resolver: yupResolver(alertSchema),
   });
+  const alerts = useSelector((state: RootState) => state.alerts);
 
   const addNewAlert = (symbol: string, alertPrice: string) => {
+    const alert = alerts.find(alertItem => alertItem.symbol === symbol);
+    if (alert) {
+      Toast.show('This alert is already at your watchlist.', Toast.LONG);
+      return;
+    }
     dispatch(addAlert({ symbol, alertPrice }));
     dispatch(
       addWatchList({ symbol, alertPrice, currentPrice: 0, percentChange: 0 }),
@@ -25,6 +33,7 @@ export const useAlert = () => {
       symbol: '',
       alertPrice: '',
     });
+    Toast.show('Alert saved.', Toast.SHORT);
   };
 
   return {
